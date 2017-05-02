5485394dcb041b76d08cf1a8548eaf21ebe54d3b
Alexandros <fotiadis90@gmail.com>
2017-05-02 11:02:50 +0300

    change exclude method: include only development (leads to less patterns and faster packaging)

--- dist/index.js
+++ dist/index.js
@@ -2,23 +2,26 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const child_process_1 = require("child_process");
 const path_1 = require("path");
-class Test {
+class ExcludeDevDependecies {
     constructor(serverless) {
         this.serverless = serverless;
-        this.hooks = {};
         this.excludeDevelopmentDependencies();
     }
     excludeDevelopmentDependencies() {
-        const output = child_process_1.execSync('npm list --dev --parseable');
+        const output = child_process_1.execSync('npm list --production --parseable');
         const devModulePaths = output.toString()
             .split('\n')
             .filter((p) => !!p.trim())
-            .map((p) => `${path_1.relative(process.cwd(), p)}/**`);
+            .map((p) => `${path_1.relative(process.cwd(), p)}/**`)
+            .filter((p) => p.indexOf('node_modules') === 0);
         if (!this.serverless.service.package)
             this.serverless['service']['package'] = {};
-        if (!this.serverless['service']['package']['exclude'])
-            this.serverless['service']['package']['exclude'] = [];
-        devModulePaths.forEach((path) => this.serverless['service']['package']['exclude'].push(path));
+        if (!this.serverless['service']['package']['include'])
+            this.serverless['service']['package']['include'] = [];
+        devModulePaths.forEach((path) => {
+            this.serverless.cli.log(`ADD PATH ${path} TO package.include`);
+            this.serverless['service']['package']['include'].push(path);
+        });
     }
 }
-module.exports = Test;
+module.exports = ExcludeDevDependecies;
--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,32 @@
 import {exec} from 'child_process';
 import {execSync} from 'child_process';
 import {relative} from 'path';
-class Test {
 
-  public hooks: any = {};
+class ExcludeDevDependecies {
 
   constructor(private serverless: any) {
     this.excludeDevelopmentDependencies();
   }
 
   excludeDevelopmentDependencies() {
-    const output = execSync('npm list --dev --parseable');
+    const output = execSync('npm list --production --parseable');
     const devModulePaths = output.toString()
       .split('\n')
       .filter((p) => !!p.trim())
-      .map((p) => `${relative(process.cwd(), p)}/**`);
+      .map((p) => `${relative(process.cwd(), p)}/**`)
+      .filter((p) => p.indexOf('node_modules') === 0);
 
     if (!this.serverless.service.package)
       this.serverless['service']['package'] = {};
-    if (!this.serverless['service']['package']['exclude'])
-      this.serverless['service']['package']['exclude'] = [];
+    if (!this.serverless['service']['package']['include'])
+      this.serverless['service']['package']['include'] = [];
 
-    devModulePaths.forEach((path) => this.serverless['service']['package']['exclude'].push(path));
+    devModulePaths.forEach((path) => {
+       this.serverless.cli.log(`ADD PATH ${path} TO package.include`);
+       this.serverless['service']['package']['include'].push(path);
+    });
   }
 }
 
 
-module.exports = Test;
+module.exports = ExcludeDevDependecies;
